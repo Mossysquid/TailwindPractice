6d134a080b96d409c8d92b8a23edd739ba4d57ab
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-09-13 19:52:35 -0600

    my npx and input work and are connect to my index.html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*{html,js}",
-    "./public/index.html"
+    "./src/*.{html,js}",
+    "index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+} 
